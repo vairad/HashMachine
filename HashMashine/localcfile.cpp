b583b5be035b88d93abb4f235392193579533cd5
Radek Vais <you@example.com>
2016-05-03 20:44:45 +0200

    class for read file using c libraries

---
+++ HashMashine/localcfile.cpp
@@ -0,0 +1,45 @@
+#include "localcfile.h"
+
+/**
+ * Konstruktor otevře soubor a nastaví základní proměnné.
+ *
+ * @param path cesta k souboru
+ */
+LocalCFile::LocalCFile(char *path){
+   file = fopen(path,"r");
+   fseek(file, 0, SEEK_END);
+   byteCount = ftell(file);
+   fseek(file, 0, SEEK_SET);
+}
+
+/**
+ * Vrátí počet bytů v souboru
+ *
+ * @return 64bit number
+ */
+uint64_t LocalCFile::getBytesCount(){
+    return byteCount;
+}
+
+/**
+ * Přečte jeden další byte ze souboru.
+ *
+ * @return 8bit number
+ */
+uint8_t LocalCFile::getByte(){
+    uint8_t tmp;
+    fread(&tmp, 1, 1, file);
+    return tmp;
+}
+
+/**
+ * Kontroluje, zda je lze ze souboru číst.
+ *
+ * @return zda lze soubor číst
+ */
+bool LocalCFile::isOk(){
+    if(file != NULL){
+        return true;
+    }
+    return false;
+}
---
+++ HashMashine/localcfile.h
@@ -0,0 +1,29 @@
+#ifndef LOCALCFILE_H
+#define LOCALCFILE_H
+
+#include <cstdio>
+
+#include "ifile.h"
+
+/**
+ * Třída která k načtení souboru využívá standardní c implementaci stdio.h.
+ * Implementuje interface IFile pro definici služeb od souboru.
+ *
+ * @author Radek VAIS
+ * @version 1.0.0
+ * @date 3.5.2016
+ */
+class LocalCFile : public IFile
+{
+    const char *name = "LocalCFile:";
+    FILE *file;
+    uint64_t byteCount;
+
+public:
+    LocalCFile(char* path);
+    uint64_t getBytesCount();
+    uint8_t getByte();
+    bool isOk();
+};
+
+#endif // LOCALCFILE_H
