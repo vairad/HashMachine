1d44fc8d0cefad636bb6c14e6ba3cf2d2ce4ebf4
Radek Vais <you@example.com>
2016-05-04 17:34:24 +0200

    error message modified

--- HashMachine/noalgorithm.cpp
+++ HashMachine/noalgorithm.cpp
@@ -7,6 +7,6 @@ std::string *NoAlgorithm::hash(IFile *file){
     if(file->isOk()){
     return new std::string("File is ok but no hash algorithm choosed");
     }else{
-        return new std::string("No algorithm choosed");
+        return new std::string("No algorithm choosed only type sha512 is implemented.");
     }
 }
